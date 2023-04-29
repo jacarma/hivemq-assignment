6612ef687148328fdf2c3bc798f30afd1aa4eb84
jacarma <jacarma@gmail.com>
2023-04-29 08:57:51 +0200

    feat: messages table

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -4,15 +4,28 @@ import { Message } from '../../Home';
 
 export default function Messages({ client, messages }: { client: MqttClient; messages: Message[] }) {
   return (
-    <div className="ml-8 w-96">
+    <div className="ml-16 w-96">
       <h2 className="text-2xl font-semibold flex-1">Messages</h2>
       <PublishMessageForm client={client} />
-      <div>
-        {messages.map(({ topic, message }, i) => (
-          <div className="border-b last:border-b-0 py-2" key={i}>
-            {topic} {message}
-          </div>
-        ))}
+      <div className="mt-4">
+        <table className="table-auto w-full">
+          <thead className="border-b">
+            <tr className="bg-gray-100">
+              <th className="text-left p-4 font-medium">Message</th>
+              <th className="text-left p-4 font-medium">Topic</th>
+              <th className="text-left p-4 font-medium">QoS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {messages.map(({ topic, message, qos }, i) => (
+              <tr key={i} className="border-b hover:bg-gray-50">
+                <td className="p-4">{message}</td>
+                <td className="p-4">{topic}</td>
+                <td className="p-4">{qos}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
