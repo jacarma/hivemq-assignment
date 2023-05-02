3f274d297252aa20dbd27c57a1d1103710c7a7e9
jacarma <jacarma@gmail.com>
2023-05-02 12:52:36 +0200

    fix: small fixes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,7 +15,10 @@ export default function Button({
         className={`inline-flex select-none justify-center rounded-md px-4 py-2 text-sm font-medium 
       ${disabled ? 'bg-gray-200' : 'bg-amber-400'} text-white hover:bg-amber-500 border border-transparent 
         focus:outline-none focus-visible:ring focus-visible:ring-amber-300 focus-visible:ring-opacity-75 `}
-        onClick={onClick}
+        onClick={(e) => {
+          e.preventDefault();
+          onClick();
+        }}
       >
         {label}
       </button>
